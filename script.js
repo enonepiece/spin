c6baa33af1be87fc0599bf0bafa9b7803524b187
Andrew <124030603+enonepiece@users.noreply.github.com>
2023-09-22 10:30:42 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -8,31 +8,31 @@ const spinValues = [
   { minDegree: 31, maxDegree: 60, value: "炒飯" },
   { minDegree: 0, maxDegree: 30, value: "乞丐時光" },
   { minDegree: 331, maxDegree: 360, value: "滷肉飯" },
-  { minDegree: 301, maxDegree: 330, value: "妳" },
+  { minDegree: 301, maxDegree: 330, value: "水喝到飽" },
   { minDegree: 271, maxDegree: 300, value: "雞腿便當" },
   { minDegree: 241, maxDegree: 270, value: "排骨便當" },
   { minDegree: 211, maxDegree: 240, value: "炒麵" },
   { minDegree: 181, maxDegree: 210, value: "牛排" },
   { minDegree: 151, maxDegree: 180, value: "湯麵" },
   { minDegree: 121, maxDegree: 150, value: "泡麵" },
-  { minDegree: 91, maxDegree: 120, value: "水喝到飽" },
+  { minDegree: 91, maxDegree: 120, value: "妳" },
 ];
 /* --------------- Size Of Each Piece  --------------------- */
 const size = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
 /* --------------- Background Colors  --------------------- */
 var spinColors = [
-  "#E74C3C",
-  "#7D3C98",
-  "#2E86C1",
-  "#138D75",
-  "#F1C40F",
-  "#D35400",
-  "#138D75",
-  "#F1C40F",
-  "#b163da",
-  "#E74C3C",
-  "#7D3C98",
-  "#138D75",
+  "#0085c3",
+  "#7ab800",
+  "#f2af00",
+  "#dc5034",
+  "#ce1126",
+  "#b7295a",
+  "#6e2585",
+  "#71c6c1",
+  "#5482ab",
+  "#009bbb",
+  "#444444",
+  "#c0c0c0",
 ];
 /* --------------- Chart --------------------- */
 /* --------------- Guide : https://chartjs-plugin-datalabels.netlify.app/guide/getting-started.html --------------------- */
@@ -40,7 +40,7 @@ let spinChart = new Chart(spinWheel, {
   plugins: [ChartDataLabels],
   type: "pie",
   data: {
-    labels: ["牛肉麵", "炒飯", "乞丐時光", "滷肉飯", "妳", "雞腿便當", "排骨便當", "炒麵", "牛排", "湯麵", "泡麵", "水喝到飽"],
+    labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
     datasets: [
       {
         backgroundColor: spinColors,
@@ -60,7 +60,7 @@ let spinChart = new Chart(spinWheel, {
         rotation: 90,
         color: "#ffffff",
         formatter: (_, context) => context.chart.data.labels[context.dataIndex],
-        font: { size: 13 },
+        font: { size: 14 },
       },
     },
   },
